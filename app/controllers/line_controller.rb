1349fba2bd5ca0ea33bcef8012dfbc3582bf592b
jiunjiun <quietmes@gmail.com>
2016-12-26 00:55:18 +0800

    Add - Add Line callback

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -1,4 +1,10 @@
 class LineController < ApplicationController
   def callback
+    unless Line::Callback.validate_signature? request
+      render status: 400, text: 'Bad Request' and return
+    else
+      Line::Callback.parse request
+      render text: "OK" and return
+    end
   end
 end
